9365baba766d570d2e91b8586f576f2800f4577a
Ted <tedbausch@gmail.com>
2018-02-07 19:58:57 -0500

    Added communication with server in a test script
    Just like it says

---
+++ Paint the town/Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿// Get the latest webcam shot from outside "Friday's" in Times Square
+using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string url = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+    IEnumerator Start()
+    {
+        using (WWW www = new WWW(url))
+        {
+            yield return www;
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material.mainTexture = www.texture;
+        }
+    }
+}
---
+++ Paint the town/Assets/TestColorCreation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestColorCreation : MonoBehaviour {
+
+    string localURL = "http://localhost:9090/api/colors";
+
+    // Use this for initialization
+    IEnumerator Start () {
+        WWWForm f = new WWWForm();
+        f.AddField("name", "Super Green");
+        f.AddField("hex", "#00ff00");
+        f.AddField("rgb[]", "0");
+        f.AddField("rgb[]", "255");
+        f.AddField("rgb[]", "0");
+
+        var test = UnityWebRequest.Post(localURL, f);
+
+        // Wait until the download is done
+        yield return test.SendWebRequest();
+
+        if (test.isNetworkError || test.isHttpError)
+        {
+            print("Error downloading: " + test.error);
+        }
+        else
+        {
+            // show the highscores
+            Debug.Log(test.downloadHandler.text);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
