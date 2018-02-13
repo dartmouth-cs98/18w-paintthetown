205acecd8dfb8f189e966787463bb960175abbb6
Alex Sheehan <ajsheehan618@gmail.com>
2018-02-13 15:38:41 -0500

    added second scene with button that takes player to original scene, will eventually use this to create the login scene

---
+++ Paint the town/Assets/Scripts/Login Scene/testLoginScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testLoginScreen : MonoBehaviour {
+
+	public void changemenuscene(string scenename){
+		Application.LoadLevel (scenename);
+
+	}
+
+}
