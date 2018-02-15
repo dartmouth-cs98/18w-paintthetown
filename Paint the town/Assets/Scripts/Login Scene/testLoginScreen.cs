7ce02563c4813879c6f19f5956134be22f8764e9
Morgan Philie <morgan@MacBook-Pro.local>
2018-02-15 17:31:55 -0500

    got user input

--- Paint the town/Assets/Scripts/Login Scene/testLoginScreen.cs
+++ Paint the town/Assets/Scripts/Login Scene/testLoginScreen.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class testLoginScreen : MonoBehaviour {
+	public GUIText  gt;
+
+	void Start()
+	{
+		gt = GetComponent<GUIText>();
+	}
 
 	public void changemenuscene(string scenename){
 		Application.LoadLevel (scenename);
 
+	}
+
+
+	public void getUserInput()
+	{
+		foreach (char c in Input.inputString)
+		{
+			if (c == '\b') // has backspace/delete been pressed?
+			{
+				if (gt.text.Length != 0)
+				{
+					gt.text = gt.text.Substring(0, gt.text.Length - 1);
+				}
+			}
+			else if ((c == '\n') || (c == '\r')) // enter/return
+			{
+				print("User entered: " + gt.text);
+			}
+			else
+			{
+				gt.text += c;
+			}
+		}
 	}
 
 }
