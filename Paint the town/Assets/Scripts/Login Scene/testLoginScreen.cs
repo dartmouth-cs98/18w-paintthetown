5dcd6b86532718475d276d2a9e52ae4340ea1fd9
Alex Sheehan <ajsheehan618@gmail.com>
2018-02-15 18:10:00 -0500

    work in progress for login page

--- Paint the town/Assets/Scripts/Login Scene/testLoginScreen.cs
+++ Paint the town/Assets/Scripts/Login Scene/testLoginScreen.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class testLoginScreen : MonoBehaviour {
 	public GUIText  gt;
 
+	string userURL = "https://paint-the-town.herokuapp.com/api/signin";
+	string localURL= "https://paint-the-town.herokuapp.com/api/signup";
+
 	void Start()
 	{
 		gt = GetComponent<GUIText>();
 	}
 
 	public void changemenuscene(string scenename){
+
+
+
 		Application.LoadLevel (scenename);
 
 	}
 
-
-	public void getUserInput()
+	public void getUserPassword()
 	{
 		foreach (char c in Input.inputString)
 		{
@@ -30,6 +35,30 @@ public class testLoginScreen : MonoBehaviour {
 			else if ((c == '\n') || (c == '\r')) // enter/return
 			{
 				print("User entered: " + gt.text);
+
+			}
+			else
+			{
+				gt.text += c;
+			}
+		}
+	}
+
+	public void getUserUsername()
+	{
+		foreach (char c in Input.inputString)
+		{
+			if (c == '\b') // has backspace/delete been pressed?
+			{
+				if (gt.text.Length != 0)
+				{
+					gt.text = gt.text.Substring(0, gt.text.Length - 1);
+				}
+			}
+			else if ((c == '\n') || (c == '\r')) // enter/return
+			{
+				print("User entered: " + gt.text);
+
 			}
 			else
 			{
