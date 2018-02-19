a7abaa52009802dbf8d76e4a9b878972b8d112ef
Alex Sheehan <ajsheehan618@gmail.com>
2018-02-19 13:45:05 -0500

    completed sign up functionality, sign in still needs work

--- Paint the town/Assets/Scripts/Login Scene/testLoginScreen.cs
+++ Paint the town/Assets/Scripts/Login Scene/testLoginScreen.cs
@@ -19,6 +19,10 @@ public class testLoginScreen : MonoBehaviour {
 	public GameObject signupPassword;
 	private string SignupUsername;
 	private string SignupPassword;
+	public GameObject signupName;
+	public GameObject signupLastName;
+	private string SignupName;
+	private string SignupLastName;
 
 	public IEnumerator SigninButton(){
 
@@ -27,11 +31,15 @@ public class testLoginScreen : MonoBehaviour {
 		f.AddField("email", Username);
 		f.AddField("password", Password);
 
+		print ("this is the password you put in: " + Password);
+		print ("this is the username you put in: " + Username);
+
 		var test = UnityWebRequest.Post(signinURL, f);
 
 		// Wait until the download is done
 		yield return test.SendWebRequest();
 
+		print (test.isHttpError);
 		if (test.isNetworkError || test.isHttpError) {
 			print ("Error downloading: " + test.error);
 		} else {
@@ -43,34 +51,6 @@ public class testLoginScreen : MonoBehaviour {
 			print (subStrings [3]);
 		}
 
-//		print ("You want to sign in!");
-//
-//		WWWForm signinform = new WWWForm();
-//
-//		signinform.AddField("email", Username);
-//		signinform.AddField("password", Password);
-//
-//		var signin = UnityWebRequest.Post(signinURL, signinform);
-//
-//		// Wait until the download is done
-//		yield return signin.SendWebRequest();
-//
-//		if (signin.isNetworkError || signin.isHttpError)
-//		{
-//			print("Error downloading: " + signin.error);
-//		}
-//		else
-//		{
-//			// show the highscores
-//			print("user signed in!");
-//			string token = signin.downloadHandler.text;
-//			string[] subStrings = token.Split ('"');
-//
-//			print(subStrings[3]);
-//			// if we want to do something with the token
-//			// string getToken = "JWT " + subStrings[3];
-//		}
-
 	}
 		
 	public IEnumerator RegisterButton(){
@@ -80,6 +60,8 @@ public class testLoginScreen : MonoBehaviour {
 
 		signupform.AddField("email", SignupUsername);
 		signupform.AddField("password", SignupPassword);
+		signupform.AddField("name", SignupName);
+		signupform.AddField("lastName", SignupName);
 
 		var signup = UnityWebRequest.Post(signupURL, signupform);
 
@@ -128,6 +110,8 @@ public class testLoginScreen : MonoBehaviour {
 		Password = password.GetComponent<InputField> ().text;
 		SignupUsername = signupUsername.GetComponent<InputField> ().text;
 		SignupPassword = signupPassword.GetComponent<InputField> ().text;
+		SignupName = signupName.GetComponent<InputField> ().text;
+		SignupLastName = signupLastName.GetComponent<InputField> ().text;
 
 	}
 }
