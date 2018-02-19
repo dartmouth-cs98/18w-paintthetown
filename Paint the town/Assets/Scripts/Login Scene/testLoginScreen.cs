88b00a1f9dc83dbef94544b44aedfde398effdcf
Morgan Philie <morgan@MacBook-Pro.local>
2018-02-18 23:05:07 -0500

    added server connection, but has bug

--- Paint the town/Assets/Scripts/Login Scene/testLoginScreen.cs
+++ Paint the town/Assets/Scripts/Login Scene/testLoginScreen.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using System.Text.RegularExpressions;
 
 public class testLoginScreen : MonoBehaviour {
 
 	// the two URL strings that we need to use
-	//	string userURL = "https://paint-the-town.herokuapp.com/api/signin";
-	//	string localURL= "https://paint-the-town.herokuapp.com/api/signup";
+	string signinURL = "https://paint-the-town.herokuapp.com/api/signin";
+	string signupURL= "https://paint-the-town.herokuapp.com/api/signup";
 
 	public GameObject username;
 	public GameObject password;
@@ -19,18 +20,73 @@ public class testLoginScreen : MonoBehaviour {
 	private string SignupUsername;
 	private string SignupPassword;
 
-	public void SigninButton(){
+	public IEnumerator SigninButton(){
 		print ("You want to sign in!");
+
+		WWWForm signinform = new WWWForm();
+
+		signinform.AddField("email", Username);
+		signinform.AddField("password", Password);
+
+		var signin = UnityWebRequest.Post(signinURL, signinform);
+
+		// Wait until the download is done
+		yield return signin.SendWebRequest();
+
+		if (signin.isNetworkError || signin.isHttpError)
+		{
+			print("Error downloading: " + signin.error);
+		}
+		else
+		{
+			// show the highscores
+			print("user signed in!");
+			string token = signin.downloadHandler.text;
+			string[] subStrings = token.Split ('"');
+
+			print(subStrings[3]);
+			// if we want to do something with the token
+			// string getToken = "JWT " + subStrings[3];
+		}
+
 	}
 		
-	public void RegisterButton(){
+	public IEnumerator RegisterButton(){
 		print ("You want to register!");
+
+		WWWForm signupform = new WWWForm();
+
+		signupform.AddField("email", SignupUsername);
+		signupform.AddField("password", SignupPassword);
+
+		var signup = UnityWebRequest.Post(signupURL, signupform);
+
+		// Wait until the download is done
+		yield return signup.SendWebRequest();
+
+		if (signup.isNetworkError || signup.isHttpError)
+		{
+			print("Error downloading: " + signup.error);
+		}
+		else
+		{
+			// show the highscores
+			print("user signed up!");
+			string token = signup.downloadHandler.text;
+			string[] subStrings = token.Split ('"');
+
+			print(subStrings[3]);
+			// if we want to do something with the token
+			// string getToken = "JWT " + subStrings[3];
+		}
+
 	}
+
 	public void Update() {
 		
 		if (Input.GetKeyDown (KeyCode.Return)) {
 			if (Password != "" && Username != "") {
-				SigninButton ();
+				SigninButton();
 			}else if (SignupPassword != "" && SignupUsername != "") {
 				RegisterButton();
 			}
