9eac95aa09ba624eb5bd794b384860a091d5f667
Ted <tedbausch@gmail.com>
2018-02-27 20:57:06 -0500

    added a lot of particle system stuff--it doesn't work with this scene yet. Fixed collision with the imported model too!

---
+++ Paint the town/Assets/ParticleSplatter/Scripts/ParticleDecalData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDecalData{
+    public Vector3 pos;
+    public float size;
+    public Vector3 rotation;
+    public Color color;
+}
