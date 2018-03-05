6975862162e238992e4f4f7d7002ea1ea1128d4b
Ted <tedbausch@gmail.com>
2018-03-05 16:41:46 -0500

    added splatter on droplet hit as well, for extra fidelity

--- Paint the town/Assets/ParticleSplatter/Scripts/ParticleDecalData.cs
+++ Paint the town/Assets/ParticleSplatter/Scripts/ParticleDecalData.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// based on https://unity3d.com/learn/tutorials/topics/scripting/droplet-decals?playlist=17117
+
+
 public class ParticleDecalData{
     public Vector3 pos;
     public float size;
