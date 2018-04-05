dc457a8ad1104c279d2b84e426e7fe3ebd24727e
Ted <tedbausch@gmail.com>
2018-04-05 18:14:26 -0400

    added some pinch-to-zoom support for the orbiting camera

--- Paint the town/Assets/orbitCamera.cs
+++ Paint the town/Assets/orbitCamera.cs
@@ -24,6 +24,8 @@ public class orbitCamera : MonoBehaviour {
     float x = 0.0f;
     float y = 0.0f;
 
+    public float zoomSpeed = .5f; // speed to zoom in or out at 
+
     // Use this for initialization
     void Start()
     {
@@ -48,14 +50,59 @@ public class orbitCamera : MonoBehaviour {
     {
         if (target && Input.touchCount > 0)
         {
-            //x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
-            //y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
-            t = Input.GetTouch(0);
+            if (Input.touchCount == 2)
+            {
+                // store them 
+                Touch touch0 = Input.GetTouch(0);
+                Touch touch1 = Input.GetTouch(1);
 
-            x += t.deltaPosition.x * xSpeed * distance * 0.002f;
-            y += t.deltaPosition.y * ySpeed * 0.002f;
+                // find the positions of those touches in the previous frame
+                Vector2 touch0PrevPos = touch0.position - touch0.deltaPosition;
+                Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
 
-            y = ClampAngle(y, yMinLimit, yMaxLimit);
+                // find magnitude of the distance between the touches, both current and in the previous frame
+                float touchDistanceMag = (touch0.position - touch1.position).magnitude;
+                float prevTouchDistanceMag = (touch0PrevPos - touch1PrevPos).magnitude;
+
+                // find the difference in magnitude between the two distances
+                float distMagnitudeDiff = prevTouchDistanceMag - touchDistanceMag;
+
+                distance += distMagnitudeDiff * zoomSpeed;
+
+                //Camera c = Camera.main;
+                //// if the camera is orthographic
+                //if (c.orthographic)
+                //{
+                //    // change the orthographic size based on the change in distance between the touches
+                //    c.orthographicSize += distMagnitudeDiff * zoomSpeed;
+
+                //    // make sure the orthographic size never goes negative
+                //    c.orthographicSize = Mathf.Max(c.orthographicSize, 0.1f);
+                //}
+
+                //// otherwise the camera is in perspective mode
+                //else
+                //{
+                //    // change the field of view based on the change in distance between the touches
+                //    c.fieldOfView += distMagnitudeDiff * zoomSpeed;
+
+                //    // clamp the fov to make sure it's between 0 and 180.
+                //    c.fieldOfView = Mathf.Clamp(c.fieldOfView, 0.1f, 179.9f);
+                //}
+
+            }
+
+            else
+            {
+                //x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+                //y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+                t = Input.GetTouch(0);
+
+                x += t.deltaPosition.x * xSpeed * distance * 0.002f;
+                y += t.deltaPosition.y * ySpeed * 0.002f;
+
+                y = ClampAngle(y, yMinLimit, yMaxLimit);
+            }
 
             Quaternion rotation = Quaternion.Euler(y, x, 0);
 
@@ -71,6 +118,7 @@ public class orbitCamera : MonoBehaviour {
 
             transform.rotation = rotation;
             transform.position = position;
+            
         }
     }
 
