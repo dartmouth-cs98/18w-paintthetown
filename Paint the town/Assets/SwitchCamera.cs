22d5335e5daee077a0607a696910ba383796ac74
Shoopalapa <wyattprill@gmail.com>
2018-04-19 19:20:46 -0400

    Final commit

---
+++ Paint the town/Assets/SwitchCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchCamera : MonoBehaviour {
+
+    public Camera povCam;
+    public Camera setCam;
+    public Button switchBtn;
+         
+	// Use this for initialization
+	void Start () {
+        Button btn = switchBtn.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        setCam.enabled = true;
+        povCam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick()
+    {
+        povCam.enabled = !povCam.enabled;
+        setCam.enabled = !setCam.enabled;
+        print("Cameras switched!");
+    }
+}
