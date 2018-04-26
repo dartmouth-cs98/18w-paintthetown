1c117cda99ace4dc89b766e8e3610a9b208fe844
Ted <tedbausch@gmail.com>
2018-04-26 16:52:12 -0400

    Added particle collision! (It just works)
    now for first person support

---
+++ Paint the town/Assets/ParticleSplatter/Scripts/particleLauncherHighlight.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// based on https://unity3d.com/learn/tutorials/topics/scripting/droplet-decals?playlist=17117
+
+
+public class particleLauncherHighlight : MonoBehaviour {
+
+    public ParticleSystem pLauncher;
+    public ParticleSystem splatterParticles;
+    public ParticleDecalPool PDP;
+
+    List<ParticleCollisionEvent> cEvents;
+
+    public string bID;
+    public string owner;
+    public string playerColor;
+
+    // Use this for initialization
+    void Start () {
+        cEvents = new List<ParticleCollisionEvent>();
+        bID = PlayerPrefs.GetString("bid");
+        playerColor = PlayerPrefs.GetString("color");
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ParticlePhysicsExtensions.GetCollisionEvents(pLauncher, other, cEvents);
+
+        for(int i = 0; i < cEvents.Count; i++)
+        {
+            PDP.particleHit(cEvents[i]);
+            EmitAtLocation(cEvents[i]);
+        }
+    }
+
+    void EmitAtLocation(ParticleCollisionEvent pce)
+    {
+        splatterParticles.transform.position = pce.intersection;
+        splatterParticles.transform.rotation = Quaternion.LookRotation(pce.normal);
+        splatterParticles.Emit(1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+}
