08f4ea6beff50e79409bb772be070e165b2e30ae
Alexander Sheehan <alexander.j.sheehan.18@dartmouth.edu>
2018-05-14 16:26:14 -0400

    forgot some files

---
+++ Paint the town/Assets/Scripts/Overworld Scene/clonedHighlight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clonedHighlight : MonoBehaviour {
+
+	public GameObject originalHighlight;
+	private Renderer rend = new Renderer();
+
+	public clonedHighlight(GameObject original){
+		originalHighlight = original;
+		rend = original.GetComponent<Renderer>();
+	}
+
+	public void setOriginal(GameObject original){
+		originalHighlight = original;
+	}
+
+	public Vector3 getOriginalCenter(){
+		rend = originalHighlight.GetComponent<Renderer>();
+		return originalHighlight.transform.position;
+	}
+
+}
