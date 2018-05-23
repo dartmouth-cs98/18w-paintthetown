d1c0ac2c9f700808cdd66c2eb73fbdccf81fd576
Morgan Philie <morgan.a.philie.18@dartmouth.edu>
2018-05-23 16:52:57 -0400

    progress on displaying grid of challenges; need array from server

--- Paint the town/Assets/Scripts/ChallengeScene.cs
+++ Paint the town/Assets/Scripts/ChallengeScene.cs
@@ -14,6 +14,8 @@ using UnityEngine.UI;
 public class ChallengeScene : MonoBehaviour {
 	public RectTransform parent;
 	public GridLayoutGroup grid;
+	public GameObject buttonCompletePrefab;
+	public GameObject buttonIncompletePrefab;
 	public string challenges;
 	public int col,row = 1;
 	public int challengeNum;
@@ -30,11 +32,41 @@ public class ChallengeScene : MonoBehaviour {
 	}
 
 	void displayChallenges() {
+		
 
+		print (challenges);
+		challenges = "{\"description\": \"This is the first challenge.\", \"completed\": \"false\", \"reward\": \"10 cool points\"}; {\"description\": \"This is the second challenge.\", \"completed\": \"true\", \"reward\": \"50 cool points\"}";
+		//print (challenges);
+		string[] challengesStringList = challenges.Split (';');
+		challengeNum = challengesStringList.Length;
+		challengesStringList [0] = challengesStringList [0].Remove (0, 1);
+		challengesStringList [challengeNum - 1] = challengesStringList [challengeNum - 1].Remove (challengesStringList [challengeNum - 2].Length, 1);
 
+		Button[] toDisplay = new Button[challengeNum];
+		GameObject tempButton;
+		Challenge tempChallenge;
+		int i = 0;
 
-		grid.cellSize = new Vector2 (parent.rect.width / col, parent.rect.height / row);
+		foreach (string challenge in challengesStringList) {
+			// cast to serializable class
+			tempChallenge = JsonUtility.FromJson<Challenge>(challenge);
 
+			// completed challenge
+			if (tempChallenge.completed == true) {
+				tempButton = (GameObject)Instantiate(buttonCompletePrefab);
+
+			} else { //incomplete challenge
+				tempButton = (GameObject)Instantiate(buttonIncompletePrefab);
+
+			}
+
+			tempButton.transform.SetParent(parent);
+			tempButton.transform.GetChild (0).GetComponent<Text> ().text = tempChallenge.description;
+			toDisplay[i] = tempButton.GetComponent<Button>();
+			i++;
+		}
+
+		// still need to do something about adjusting the number of rows as number of challenges increases?
 
 
 	}
