6b3945fd7ce09ac2673ca6a1453fc31705899f9e
Morgan Philie <morgan.a.philie.18@dartmouth.edu>
2018-05-29 22:09:29 -0400

    reformatted ShowTextBox script

--- Paint the town/Assets/Scripts/Overworld Scene/ShowTextBox.cs
+++ Paint the town/Assets/Scripts/Overworld Scene/ShowTextBox.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class ShowTextBox : MonoBehaviour {
  	public Text textArea;
 	public Image image;
@@ -16,22 +15,19 @@ public class ShowTextBox : MonoBehaviour {
 	void Start () {
 		image.enabled = false;
 		textArea.enabled = false;
-		//strings[0] = "ERROR: MESSAGE NOT SET";
 	}
 
-	// STUFF
+
 	IEnumerator displayTimer(){
 		while(true){
-
 			if((Input.touchCount == 1 || Input.GetKeyDown(KeyCode.Space)) && index == (strings.Length)){
-
 				break;
 			}
 
 			yield return new WaitForSeconds(speed);
 
 			if(characterIndex >= strings[index].Length){
-        textArea.text = strings[index].Substring(0, characterIndex);
+        		textArea.text = strings[index].Substring(0, characterIndex);
 				continue;
 			}
 
@@ -49,6 +45,7 @@ public class ShowTextBox : MonoBehaviour {
 		StartCoroutine("displayTimer");
 	}
 
+	// update every frame to show the text box with the correct text for the timer
 	void Update () {
 		if((Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetKeyDown(KeyCode.Space)){
 			if(image.enabled == true && textArea.enabled == true){
@@ -57,8 +54,8 @@ public class ShowTextBox : MonoBehaviour {
 					textArea.enabled = false;
 					index = 0;
 					characterIndex = 0;
-        } else if(characterIndex < strings[index].Length && index < strings.Length){
-          characterIndex = strings[index].Length;
+        		} else if(characterIndex < strings[index].Length && index < strings.Length){
+        			characterIndex = strings[index].Length;
 
 				}else if (index < strings.Length){
 					index++;
